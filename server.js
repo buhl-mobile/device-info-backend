c2c3871a3062c93d57fea7aafb3f5783ac54e493
buhl-mobile <service@buhl-mobile.de>
2025-01-20 15:09:38 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'); // Express importieren
 const app = express(); // Express-Anwendung erstellen
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 // Middleware hinzufügen, um JSON-Daten zu parsen
 app.use(express.json());
